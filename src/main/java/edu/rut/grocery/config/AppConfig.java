09051592c347019269b1a8092b612adf2c4731c6
nn17 <yakor356@gmail.com>
2024-10-28 03:26:25 +0300

    feature: add modelmapper bean

---
+++ src/main/java/edu/rut/grocery/config/AppConfig.java
@@ -0,0 +1,14 @@
+package edu.rut.grocery.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+	@Bean
+	public ModelMapper modelMapperBean() {
+		return new ModelMapper();
+	}
+}
